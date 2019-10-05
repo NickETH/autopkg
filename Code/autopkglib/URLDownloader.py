272fe6303c6dd8e53e1a33df44399ddc0b556fc1
Heim  Niklaus (ID BD) <heim@ethz.ch>
2019-10-06 00:35:45 +0200

    AutoPkg.win 1.0.4

--- Code/autopkglib/URLDownloader.py
+++ Code/autopkglib/URLDownloader.py
@@ -67,9 +67,8 @@ def default_curl_path():
     if is_mac():
         return '/usr/bin/curl'
     if is_windows():
-        return "C:\\Program Files\\Git\\mingw64\\bin\\curl.exe"
-        # we use the curl, which comes with git.
-        # we should consider to use the CURL_PATH variable here.
+        return "C:\Program Files\curl\i386\curl.exe"
+
 
 class URLDownloader(Processor):
     """Downloads a URL to the specified download_dir using curl."""
